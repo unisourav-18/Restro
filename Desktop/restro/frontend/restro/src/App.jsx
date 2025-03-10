d1759e6107e90f05153bbcdd2af3e570b0c6baeb
Sourav <thesouravpandey108@gmail.com>
2025-03-10 12:51:31 +0530

    hello from first commit

---
+++ Desktop/restro/frontend/restro/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <><h1 className='bg-red-700 flex-between w-screen h-screen'>
+      hello
+    </h1>
+    
+    </>
+  )
+}
+
+export default App
